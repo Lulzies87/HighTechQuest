08e979fd630e8f43e756eb04693df42f2ef900ce
Lilach E <lilachelayza@gmail.com>
2025-04-16 17:32:06 +0300

    update constants

--- app/src/game/utils/Constants.ts
+++ app/src/game/utils/Constants.ts
@@ -1,10 +1,11 @@
 export const PLAYER_SPEED: number = 160;
 export const COIN_SCORE = 10;
 
-export const WORLD_WIDTH: number = 3840;
-export const WORLD_HEIGHT: number = 2160;
+export const WORLD_WIDTH: number = 2560;
+export const WORLD_HEIGHT: number = 1280;
 
-export const TILE_SIZE = 32;
+export const TILE_WIDTH = 32;
+export const TILE_HEIGHT = 32;
 
 export const UI_COLORS = {
   HEALTH_BAR_BG: 0x888888,
