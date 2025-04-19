7d8d2f0dbc02e6037058eea066b11679ff76b02b
Gabi Delcea <vioriiiee@gmail.com>
2025-04-19 22:21:08 +0300

    dev - db set back

--- api/src/dbConnection.ts
+++ api/src/dbConnection.ts
@@ -1,20 +1,20 @@
-import mongoose from "mongoose";
-
-export async function connectToDB() {
-  if (!process.env.CONN_STRING) {
-    console.warn("No connection string provided. Skipping database connection.");
-    return;
-  }
-
-  await mongoose.connect(process.env.CONN_STRING);
-}
-
 // import mongoose from "mongoose";
 
 // export async function connectToDB() {
 //   if (!process.env.CONN_STRING) {
-//     throw new Error("Must provide a connection string");
+//     console.warn("No connection string provided. Skipping database connection.");
+//     return;
 //   }
 
 //   await mongoose.connect(process.env.CONN_STRING);
 // }
+
+import mongoose from "mongoose";
+
+export async function connectToDB() {
+  if (!process.env.CONN_STRING) {
+    throw new Error("Must provide a connection string");
+  }
+
+  await mongoose.connect(process.env.CONN_STRING);
+}
